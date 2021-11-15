b216d1a6767d1076a3c75be60c99edc814e1f426
diego boy <diegolive5000@gmail.com>
2021-11-15 15:17:30 +0100

    create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "poll#index"
+  root "about#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
